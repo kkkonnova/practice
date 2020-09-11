1d9c2ffa4c2f076f7da437f062f4938526aace91
kkkakon <k.a.konnova@mail.ru>
2020-09-11 22:33:17 +0400

    Задание 1.3. Создан класс Point

---
+++ src/test/java/ru/ssau/tk/kkkonnova/practice/Point.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.kkkonnova.practice;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+
+    Point(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
