b2a89b5158c255cf7cd9beb741fd700c95145b9f
kkkakon <k.a.konnova@mail.ru>
2020-09-11 22:39:18 +0400

    Задание 1.3. Завершено. Создано несколько точек

--- src/test/java/ru/ssau/tk/kkkonnova/practice/MainClass.java
+++ src/test/java/ru/ssau/tk/kkkonnova/practice/MainClass.java
@@ -4,14 +4,9 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        Person Kristina = new Person();
-        Person Kiryusha = new Person("Kirill", "Konnov");
-        Person Stas = new Person(380107581);
-        Person Marinochka = new Person("Marina","Vershinina",361252482);
+        Point myPoint1 = new Point(0,0,0);
+        Point myPoint2 = new Point(34,56,2);
+        Point myPoint3 = new Point(4,6,7);
 
-        System.out.println(Kristina.getFirstName() + ' ' + Kristina.getLastName() + ' '+ Kristina.getPassportId());
-        System.out.println(Kiryusha.getFirstName() + ' ' + Kiryusha.getLastName() + ' '+ Kiryusha.getPassportId());
-        System.out.println(Stas.getFirstName() + ' ' + Stas.getLastName() + ' '+ Stas.getPassportId());
-        System.out.println(Marinochka.getFirstName() + ' ' + Marinochka.getLastName() + ' '+ Marinochka.getPassportId());
     }
 }
