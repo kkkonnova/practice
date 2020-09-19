3f4dfa3c900a33eee29919329d9c806536df34d1
kkkakon <k.a.konnova@mail.ru>
2020-09-19 10:42:21 +0400

    Задание 1.8. Классу Points  добавлен метод vectorProduct(), проверена его работоспособность

--- src/test/java/ru/ssau/tk/kkkonnova/practice/MainClass.java
+++ src/test/java/ru/ssau/tk/kkkonnova/practice/MainClass.java
@@ -8,8 +8,10 @@ public class MainClass {
         Point myPoint2 = new Point(3, 5, 2);
         Point myPoint3 = new Point(-4, -6, 8);
 
-        double resPoint1 = Points.scalarProduct(myPoint3, myPoint2);
-        System.out.println(resPoint1);
+        Point resPoint1 = Points.vectorProduct(myPoint2, myPoint3);
+        System.out.println(resPoint1.x);
+        System.out.println(resPoint1.y);
+        System.out.println(resPoint1.z);
 
 
     }
