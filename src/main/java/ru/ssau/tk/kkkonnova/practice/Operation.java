717dd9f14f0a354fd06c185eb80888c8f228939e
kkkakon <k.a.konnova@mail.ru>
2020-09-21 15:02:27 +0400

    1.15 Реализован метод applyTriple

--- src/main/java/ru/ssau/tk/kkkonnova/practice/Operation.java
+++ src/main/java/ru/ssau/tk/kkkonnova/practice/Operation.java
@@ -1,6 +1,10 @@
 package ru.ssau.tk.kkkonnova.practice;
 
 public abstract class Operation {
-   abstract double apply(double number);
-   abstract double applyTriple(double number);
+    abstract double apply(double number);
+
+    double applyTriple(double number) {
+        return apply(apply(apply(number)));
+    }
+
 }
