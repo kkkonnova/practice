81e150180f428b81ed345b04c2b5502066da2d4c
kkkakon <k.a.konnova@mail.ru>
2020-09-21 14:55:56 +0400

    1.6 Добавлены два конструктора класса NamedPoint

--- src/test/java/ru/ssau/tk/kkkonnova/practice/NamedPoint.java
+++ src/test/java/ru/ssau/tk/kkkonnova/practice/NamedPoint.java
@@ -3,11 +3,22 @@ package ru.ssau.tk.kkkonnova.practice;
 public class NamedPoint extends Point {
     private String name;
 
+    NamedPoint(double x, double y, double z) {
+        super(x, y, z);
+    }
+
+    NamedPoint(double x, double y, double z, String name) {
+        super(x, y, z);
+        this.name = name;
+    }
+
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 }
