2d13c6ab39552e867013807fe23eb54b8927625f
kkkakon <k.a.konnova@mail.ru>
2020-09-28 21:31:11 +0400

    3.2 Завершено. Создан метод getStringBytes()

--- src/main/java/ru/ssau/tk/kkkonnova/practice_str/MyStringClass.java
+++ src/main/java/ru/ssau/tk/kkkonnova/practice_str/MyStringClass.java
@@ -2,18 +2,28 @@ package ru.ssau.tk.kkkonnova.practice_str;
 
 public class MyStringClass {
 
+    public static void main(String[] args) {
+
+        String s1 = "Кристина";
+        String s2 = "Stas";
+        MyStringClass MyString = new MyStringClass();
+        MyString.getStringBytes(s1);
+        MyString.getStringBytes(s2);
+    }
+
     void splitLine(String myString) {
         for (int i = 0; i < myString.length(); i++) {
             System.out.println(myString.charAt(i));
         }
     }
 
+    void getStringBytes(String myString) {
+        byte[] stringBytes = myString.getBytes();
+        for (int bytes:stringBytes ){
+            System.out.println(bytes);
+        }
 
-    public static void main(String[] args) {
-
-        String s1 = "123fgh123!";
-        MyStringClass MyString = new MyStringClass();
-        MyString.splitLine(s1);
     }
+
 
 }
