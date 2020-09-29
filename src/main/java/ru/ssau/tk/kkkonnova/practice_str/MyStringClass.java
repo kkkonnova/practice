06cbcc9088d1299947f93f142bdf645c215ab470
kkkakon <k.a.konnova@mail.ru>
2020-09-29 15:08:23 +0400

    3.6. Завершено. Создан метод escapeCharacter()

--- src/main/java/ru/ssau/tk/kkkonnova/practice_str/MyStringClass.java
+++ src/main/java/ru/ssau/tk/kkkonnova/practice_str/MyStringClass.java
@@ -4,16 +4,32 @@ public class MyStringClass {
 
     public static void main(String[] args) {
 
-        String s1 = "123";
-        String s2 = "PAPA";
-        MyStringClass MyString = new MyStringClass();
-        boolean res = MyString.differInRegister(s1, s2);
-        System.out.println(res);
+        escapeCharacter();
     }
 
     boolean differInRegister(String s1, String s2) {
+
         return s1.equalsIgnoreCase(s2) && !s1.equals(s2);
     }
+
+    static void escapeCharacter() {
+        int i = 0;
+        //табуляция
+        System.out.println("Символ\t№" + ++i);
+        //удаление последнего элемента в строке
+        System.out.println("Символ\b№" + ++i);
+        //переход на новую строку
+        System.out.println("Символ\n№" + ++i);
+        //игнорирование текста до \r
+        System.out.println("Символ\r№" + ++i);
+        //символ '
+        System.out.println("Символ\'№" + ++i);
+        //символ "
+        System.out.println("Символ\"№" + ++i);
+        // символ /
+        System.out.println("Символ\\№" + ++i);
+    }
+
 
     boolean palindromeCheck(String myString) {
         String palindrome = new StringBuilder(myString).reverse().toString();
