1fee54f6a24c809adb9c43413997095c1d204cff
kkkakon <k.a.konnova@mail.ru>
2020-10-02 18:04:20 +0400

    2.3. Завершено. Создан метод generateOddNumbers()

--- src/main/java/ru/ssau/tk/kkkonnova/practice_array/myArray.java
+++ src/main/java/ru/ssau/tk/kkkonnova/practice_array/myArray.java
@@ -2,6 +2,16 @@ package ru.ssau.tk.kkkonnova.practice_array;
 
 public class myArray {
 
+    static int[] generateOddNumbers(int n) {
+        int[] array = new int[n];
+        int k = 1;
+        for (int i = 0; i < n; i++) {
+            array[i] = k;
+            k = k + 2;
+        }
+        return array;
+    }
+
     static int[] generateArrayWithOnes(int n) {
         int[] array = new int[n];
         for (int i = 0; i < n; i++) {
--- src/test/java/ru/ssau/tk/kkkonnova/practice_array/myArrayTest.java
+++ src/test/java/ru/ssau/tk/kkkonnova/practice_array/myArrayTest.java
@@ -22,4 +22,11 @@ public class myArrayTest {
         assertEquals(myArray.generateArrayWithOnes(5), new int[]{2, 1, 1, 1, 2});
 
     }
+
+    @Test
+    public void testGenerateOddNumbers() {
+        assertEquals(myArray.generateOddNumbers(2), new int[]{1,3});
+        assertEquals(myArray.generateOddNumbers(1), new int[]{1});
+        assertEquals(myArray.generateOddNumbers(5), new int[]{1,3,5,7,9});
+    }
 }
